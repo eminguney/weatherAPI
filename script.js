d9fc2f2e32f7c6b82d1be9df46c0597aa4342158
EminGuney <eminnguney@gmail.com>
2023-02-16 17:18:56 +0300

    Added automatic location finder

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const url='https://api.openweathermap.org/data/2.5/';
+const url='http://api.openweathermap.org/data/2.5/';
 const key= 'beffb96027d5fa2fc75446706367684e';
 const country= 'TR';
+
 
 const setQuery= (e) => {
     if(e.keyCode == '13')
@@ -35,3 +36,34 @@ const displayResult = (result) => {
 
 const searchBar = document.getElementById('searchBar');
 searchBar.addEventListener('keypress', setQuery)
+
+
+
+
+document.getElementById('konum').addEventListener('click',()=>{
+    if(navigator.geolocation){
+        console.log("Geolocation'ı destekliyor...")
+        navigator.geolocation.getCurrentPosition(onSuccess, onError)
+    }else{
+        console.log("Tarayıcınız geolocation'ı desteklemiyor...")
+    }
+})
+
+function fetchData(){
+    infoTxt.innerText = "Sonuçlar getiriliyor..."
+    infoTxt.classList.add("pending")
+    fetch(api).then(response => response.json()).then(displayResult)
+}
+function onSuccess(position){
+    const {latitude, longitude} = position.coords;
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${key}&lang=tr`;
+    fetchData()
+}
+
+function onError(error){
+    infoTxt.innerText = error.message   
+    infoTxt.classList.add("error")
+}
+
+const infoTxt = document.querySelector(".info-txt");
+const inputPart = document.querySelector(".input-part");
